9bcae398e32a6b24ad81a754c162308ec4e53953
Italo Sebastian Verona Flores <verona.flores.italo@gmail.com>
2025-10-27 11:59:45 -0500

    feat: refactor main.js to integrate i18n and enhance PrimeVue component usage

--- src/main.js
+++ src/main.js
@@ -1,15 +1,59 @@
-import { createApp } from "vue";
-import App from "./app.vue";
-import { router } from "./shared/router/index.js";
+import { createApp } from 'vue'
+import './style.css'
+import App from './app.vue'
+import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import {
+    Avatar,
+    Button,
+    Card,
+    Checkbox,
+    Column,
+    ConfirmationService,
+    ConfirmDialog, DataTable, Dialog,
+    DialogService, Drawer, FileUpload, FloatLabel, IconField, InputIcon, InputNumber, InputText, Menu,
+    Rating, Row, Select, SelectButton, Tag, Textarea, Toast,
+    ToastService, Toolbar, Tooltip
+} from "primevue";
+import router from "./router.js";
+import pinia from "./pinia.js";
 
-import PrimeVue from "primevue/config";
-import "primevue/resources/themes/aura-light-blue/theme.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
 
-const app = createApp(App);
-app.use(router);
-app.use(PrimeVue);
-app.mount("#app");
-
-
+// noinspection JSCheckFunctionSignatures
+createApp(App)
+    .use(i18n)
+    .use(PrimeVue, { theme: { preset: Aura}, ripple: true })
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-select', Select)
+    .component('pv-select-button', SelectButton)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-text', InputText)
+    .component('pv-input-number', InputNumber)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-drawer', Drawer)
+    .component('pv-tag', Tag)
+    .component('pv-textarea', Textarea)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast)
+    .component('pv-avatar', Avatar)
+    .directive('tooltip', Tooltip)
+    .use(router)
+    .use(pinia)
+    .mount('#app')
